fa6efa8fa8fda44e7bfca97b71b6c62d5ab38678
KJEKcode <kurtiskovel@gmail.com>
2021-04-25 15:07:41 -0400

    simple html css amd js setup

---
+++ js/compiled.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const navList = document.getElementById("nav-list");
+    if (window.innerWidth > 1025) {
+        navList.hidden = false;
+    }
+});
+
+window.addEventListener("resize", () => {
+    const navList = document.getElementById("nav-list");
+    if (window.innerWidth > 1025) {
+        navList.hidden = false;
+    } else if (window.innerWidth < 1025) {
+        navList.hidden = true;
+    }
+})
+
+
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", (event) => {
+    const ariaControls = event.currentTarget.getAttribute("aria-controls");
+    const controlledAria = document.getElementById(ariaControls);
+    const menuSwitch = document.querySelector(".menu-switch");
+    if (controlledAria.hidden) {
+        controlledAria.hidden = false;
+        event.currentTarget.setAttribute("aria-expanded", "true");
+        menuSwitch.innerHTML = "close";
+    } else if (!controlledAria.hidden) {
+        controlledAria.hidden = true;
+        event.currentTarget.setAttribute("aria-expanded", "false");
+        menuSwitch.innerHTML = "open";
+    }
+});
+
+
+
+
+    
+
